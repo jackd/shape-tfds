2591e2643fb04a824bdaba5396782080347ea4c8
Dominic Jack <thedomjack@gmail.com>
2019-08-13 10:08:32 +1000

    modelnet sampled fixes

---
+++ example/sampled.py
@@ -0,0 +1,10 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import tensorflow_datasets as tfds
+from shape_tfds.shape.modelnet import sampled
+
+builder = sampled.ModelnetSampled(config=sampled.get_config(10))
+config = tfds.download.DownloadConfig(register_checksums=True)
+builder.download_and_prepare(download_config=config)
--- shape_tfds/shape/modelnet/sampled.py
+++ shape_tfds/shape/modelnet/sampled.py
@@ -13,27 +13,35 @@ class ModelnetSampledConfig(base.CloudNormalConfig):
 
     def __init__(self, num_classes):
         """num_classes must be 10 or 40."""
-        assert(num_classes in (10, 40))
+        assert (num_classes in (10, 40))
         num_points = 10000
+        self._num_classes = num_classes
         super(ModelnetSampledConfig, self).__init__(
-                name="presampled%d-%d" % (num_classes, num_points),
-                num_points=num_points,
-                description=(
-                    "%d-class sampled 10000-point cloud used by PointNet++"
-                    % num_classes),
-                version=tfds.core.utils.Version(0, 0, 1))
+            name="presampled%d-%d" % (num_classes, num_points),
+            num_points=num_points,
+            description=(
+                "%d-class sampled 10000-point cloud used by PointNet++" %
+                num_classes),
+            version=tfds.core.utils.Version("0.0.1"))
 
     @property
     def num_classes(self):
-        return self._nun_classes
+        return self._num_classes
 
     def load_example(self, path):
         raise NotImplementedError
 
 
+_BUILDER_CONFIGS = {n: ModelnetSampledConfig(num_classes=n) for n in [10, 40]}
+
+
+def get_config(num_classes=40):
+    return _BUILDER_CONFIGS[num_classes]
+
+
 class ModelnetSampled(tfds.core.GeneratorBasedBuilder):
     URLS = [base._URL_BASE, "http://stanford.edu/~rqi/pointnet2/"]
-    BUILDER_CONFIGS = [ModelnetSampledConfig(num_classes=n) for n in [10, 40]]
+    BUILDER_CONFIGS = [_BUILDER_CONFIGS[n] for n in (10, 40)]
     _CITATION = """\
 @article{qi2017pointnetplusplus,
     title={PointNet++: Deep Hierarchical Feature Learning on Point Sets in a Metric Space},
@@ -42,6 +50,7 @@ class ModelnetSampled(tfds.core.GeneratorBasedBuilder):
     year={2017}
 }
 """
+
     @property
     def num_classes(self):
         return self.builder_config.num_classes
@@ -74,21 +83,23 @@ class ModelnetSampled(tfds.core.GeneratorBasedBuilder):
         out = []
         num_classes = self.num_classes
         for split, key, num_shards in (
-                    (tfds.Split.TRAIN, "train", 4 * num_classes // 10),
-                    (tfds.Split.TEST, "test", 1 * num_classes // 10),
-                ):
-            split_path = os.path.join(
-                data_dir, "modelnet%d_%s.txt" % (num_classes, key))
-            out.append(
-                tfds.core.SplitGenerator(
-                    name=split,
-                    num_shards=num_shards,
-                    gen_kwargs=dict(split_path=split_path, data_dir=data_dir)))
+            (tfds.Split.TRAIN, "train", 4 * num_classes // 10),
+            (tfds.Split.TEST, "test", 1 * num_classes // 10),
+        ):
+            split_path = os.path.join(data_dir,
+                                      "modelnet%d_%s.txt" % (num_classes, key))
+            with tf.io.gfile.GFile(split_path, "r") as fp:
+                example_ids = [l for l in fp.read().split("\n") if l != ""]
+            gen = tfds.core.SplitGenerator(name=split,
+                                           num_shards=num_shards,
+                                           gen_kwargs=dict(
+                                               example_ids=example_ids,
+                                               data_dir=data_dir))
+            gen.split_info.statistics.num_examples = len(example_ids)
+            out.append(gen)
         return out
 
-    def _generate_examples(self, split_path, data_dir):
-        with tf.io.gfile.GFile(split_path, "r") as fp:
-            example_ids = [l for l in fp.read().split("\n") if l != ""]
+    def _generate_examples(self, example_ids, data_dir):
         for example_id in example_ids:
             split_id = example_id.split("_")
             label = "_".join(split_id[:-1])
@@ -96,6 +107,7 @@ class ModelnetSampled(tfds.core.GeneratorBasedBuilder):
             path = os.path.join(data_dir, label, "%s.txt" % example_id)
             with tf.io.gfile.GFile(path, "rb") as fp:
                 data = np.loadtxt(fp, delimiter=",", dtype=np.float32)
-            positions, normals = np.split(data, 2, axis=1)    # pylint: disable=unbalanced-tuple-unpacking
+            positions, normals = np.split(data, 2, axis=1)  # pylint: disable=unbalanced-tuple-unpacking
             cloud = dict(positions=positions, normals=normals)
-            yield dict(cloud=cloud, label=label, example_index=example_index)
+            yield ((label, example_index),
+                   dict(cloud=cloud, label=label, example_index=example_index))
