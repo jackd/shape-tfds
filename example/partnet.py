bb8a3e659e2646be9a5b8f33cb2ad94b184e9b5e
Dominic Jack <thedomjack@gmail.com>
2019-09-12 17:27:25 +1000

    added up_dim

--- example/partnet.py
+++ example/partnet.py
@@ -7,3 +7,24 @@ from shape_tfds.shape.shapenet.partnet import Partnet
 
 for config in Partnet.BUILDER_CONFIGS:
     Partnet(config=config).download_and_prepare()
+
+# def vis():
+#     import trimesh
+#     import numpy as np
+#     colors = np.array([
+#         [255, 0, 0],
+#         [0, 255, 0],
+#         [0, 0, 255],
+#         [255, 255, 0],
+#         [255, 0, 255],
+#         [0, 255, 255],
+#     ])
+#     builder = Partnet(config='chair', level=1)
+#     assert (builder.num_classes[1] <= len(colors))
+#     dataset = builder.as_dataset(split='train', as_supervised=True)
+#     for coords, labels in tfds.as_numpy(dataset):
+#         trimesh.PointCloud(coords, colors=colors[labels]).show()
+#         coords[..., 1] = 0
+#         trimesh.PointCloud(coords, colors=colors[labels]).show()
+
+# vis()
--- shape_tfds/shape/shapenet/partnet/__init__.py
+++ shape_tfds/shape/shapenet/partnet/__init__.py
@@ -110,6 +110,10 @@ class Partnet(tfds.core.GeneratorBasedBuilder):
 
     BUILDER_CONFIGS = [PartnetConfig(c) for c in OBJECT_CATEGORIES]
 
+    @property
+    def up_dim(self):
+        return 1
+
     @core_utils.memoized_property
     def class_names(self):
         return {
